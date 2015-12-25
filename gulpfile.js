d5663187622bd25caec13a93691bd1f36ba5ecf8
cbping <452775680@qq.com>
2015-12-25 16:55:47 +0800

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+gulp.task('js', function () {
+    return gulp.src('src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(concat('player-min.js'))
+        .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('default', ['js']);
